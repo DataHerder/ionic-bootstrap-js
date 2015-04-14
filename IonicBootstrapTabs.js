2aba6ef7fda568a5f39af0caffb4a4c1c2741b64
Paul Carlton <paul.b.carlton@gmail.com>
2015-04-13 23:25:54 -0700

    0.2 update using inheritance to add on IonicBootstrap changed to instantiated class with inheritance Completely takes care of loading Ionic tabs at this point

---
+++ IonicBootstrapTabs.js
@@ -0,0 +1,180 @@
+
+/**
+ * Ionic Bootstrap Tabs Class
+ *
+ * Helps modularize code for larger hybrid
+ * projects that have tab states baked in.
+ *
+ * By Paul Carlton http://www.paulcarlton.com
+ *
+ * MIT Licensed
+ */
+
+
+var IonicBootstrapTabs = IonicBootstrap.extend({
+	root_index: '/tab/dash',
+	init: function(app_name) {
+		if (app_name == undefined || typeof(app_name) != 'string' || app_name == '') {
+			throw new Error('Invalid name for your ionic application.  ' +
+			'When instantiating IonicBootstrapTabs, please pass an app name as a string value.  ' +
+			'Ie: "var Iot = new IonicBootstrapTabs("myApp")');
+		}
+		this._super(app_name, 'IonicTabs');
+		this._loadAngularModules();
+	},
+
+	config: function(func)
+	{
+		var cb = func || false;
+		if (this.__isFunc(cb)) {
+			document.App.config(func);
+		} else {
+			document.App.config(function($stateProvider) {
+				// Ionic uses AngularUI Router which uses the concept of states
+				// Learn more here: https://github.com/angular-ui/ui-router
+				// Set up the various states which the app can be in.
+				// Each state's controller can be found in controllers.js
+				$stateProvider
+
+					// setup an abstract state for the tabs directive
+					.state('tab', {
+						url: "/tab",
+						abstract: true,
+						templateUrl: "templates/tabs.html"
+					})
+			});
+		}
+		return this;
+	},
+
+
+	states: function(state_object)
+	{
+		this.__states = state_object || {};
+		return this;
+	},
+
+	bootstrap: function(callback)
+	{
+		var cb = callback || false;
+		this._parseStates();
+		this._runList(this.__loaders, 'js', this);
+		this._runList(this.list_of_modules, 'js', this);
+		console.log(this.list_of_modules);
+		this._run(cb);
+	},
+
+	_run: function(cb)
+	{
+		var self = this;
+
+		this._runList(this.list_of_modules, 'js', self);
+		require(this.require_list, function() {
+			document.App.config(function($stateProvider, $urlRouterProvider) {
+				for (var i = 0; i < self.state_objects.length; i++) {
+					var obj = self.state_objects[i];
+					if (self.__debug_log === true) {
+						console.log(obj);
+					}
+					$stateProvider.state(obj['name'], obj['params']);
+				}
+				$urlRouterProvider.otherwise(self.root_index);
+			});
+			if (cb) {
+				cb(function(){
+					angular.bootstrap(document.getElementById(self.app_name), [self.app_name])
+				});
+			} else {
+				angular.bootstrap(document.getElementById(self.app_name), [self.app_name])
+			}
+		});
+	},
+
+	state_objects: [],
+	/**
+	 * Create the state objects from the passed states
+	 *
+	 * @private
+	 */
+	_parseStates: function() {
+		for (var root_state in this.__states) {
+			if (this.__states.hasOwnProperty(root_state)) {
+				for (var dir in this.__states[root_state]) {
+					if (this.__states[root_state].hasOwnProperty(dir)) {
+						for (var i = 0; i < this.__states[root_state][dir].length; i++) {
+							// the raw state, this is the template and controller name
+							// derive the state name (which could be anything) and
+							// scrube the controller name out of the html template
+							var _state_ = this.__states[root_state][dir][i],
+								url = '',
+								template_name = '',
+								controller_name = '',
+								state_name = '',
+								resolve = false
+							;
+							if (this._getType(_state_) === 'string') {
+								url =
+								template_name = _state_.replace(/(\/?\:.*)/, '').replace(/\/$/, '');
+								controller_name = template_name.replace(/-/g, ' ')
+									.__ionic_capitalize__()
+									.replace(/\s/g, '') + 'Ctrl';
+								// state object specific information
+								// states must have tab appended to it, state name
+								// will continue in as the controller name with a tab
+								state_name = 'tab.' + controller_name;
+							} else if (this._getType(_state_) === 'object') {
+								url = _state_.url;
+								template_name = _state_.template;
+								controller_name = _state_.controller;
+								state_name = 'tab.' + controller_name;
+								if (_state_.resolve != undefined) {
+									resolve = _state_.resolve;
+								}
+							} else {
+								throw new Error('Unsupported state type: ' + this._getType(_state_));
+							}
+
+							// override the template if it is found
+							var template_replaced = false;
+							if (/::/.test(this.__states[root_state][dir][i])) {
+								var tmp = this.__states[root_state][dir][i].split('::');
+								url = tmp[0];
+								template_replaced = true;
+								template_name = tmp[1];
+							}
+
+							var temp_state = {
+								'name': state_name,
+								params: {
+									// add the directory in at the last stage
+									'url': ('/' + dir + '/' + url).replace(/\/{2,}/, '/'),
+									views: {}
+								}
+							};
+							temp_state['params']['views'][root_state] = {
+								// the template checks to see if the root directory is being overridden simply by checking for the /
+								templateUrl: ('templates/' + (template_replaced && /\//.test(template_name) ? '' : dir + '/') + template_name + '.html').replace(/\/{2,}/, '/'),
+								controller: controller_name
+							};
+							if (resolve) {
+								temp_state['params']['resolve'] = resolve;
+							}
+							this.state_objects.push(temp_state);
+
+							// push the module to load in require
+							if (this.list_of_modules[0] == undefined) {
+								this.list_of_modules[0] = {
+									'controllers': []
+								};
+							}
+							this.list_of_modules[0].controllers.push(
+								dir + '/' + controller_name + '.js'
+							)
+						}
+					}
+				}
+			}
+		}
+	}
+
+});
--- README.md
+++ README.md
@@ -1,40 +1,74 @@
 # ionic-bootstrap-js
-Bootstrap Ionic with Require.js
-Beta v0.1
+Ionic Bootstrap with Require.js
+Beta v0.2
 
-## Automation
+##About
+This is a light weight library that utilizes require to help modularize larger ionic projects.  This version currently supports ionic tab projects.  It handles the tab stack while enforcing 1 to 1 mapping between the controller, template and url.
 
-Automates states to controllers to templates and helps modularize your code.
+You can map multiple controllers to the same template using the `::` separator.  This makes it handy to allow one template to handle different endpoints.  For instance, you may have an event template but where that template is accessed may differ depending on the stack.  For instance you may have a search tab and an event tab, but the search tab also displays an event.  This is where one template can be referenced in 2 different state stacks.  
 
-This currently automates tabbed ionic projects
+Basically, each tab is a state stack.  Handling that in one large file, or even breaking it apart into files can be cumbersome after it gets big.  What if the client comes back with a change that requires a different structure?  What about maintenance or "version 2"?  Making adjustments, adding on, or changing the stack is now much easier and handled in one spot rather than changing multiple files.
 
-Example:
+##How to
+
+Currently there is a working ionic tab application in the repo to help you get started.  You can use that as a reference.
+
+Adding into an already existing application may take some time.  It will force files to exist (because of require) with your application controller logic.  Dropping it in with no states and to slowly add states using the variable is the best way to roll out the bootstrap in an existing project.  I highly recommend using Google Chrome Developer Tools.
+
+###Step 1
+`npm install ionic -g`
+
+###Step 2
+`ionic start new_project tabs`
+
+###Step 3
+Download and place in your js/vendors directory ionic-bootstrap.js and require.js
+
+###Step 4
+Use the working example as a guide to comment out the correct scripts and copy the state stack
+
+
+##The Code
 
 ```javascript
-var bootstrap = {
-  states: {
-    // under the history stack 'tab.main-screens'
-    // loads /js/controllers/MainScreen1Ctrl.js
-    // instantiates the state object for MainScreen1
-    'main-screens': {
-      'main': ['main-screen-1']
-    },
-    'search': {
-      // search with search_id parameter in url, map to search_template.html
-      'search': ['search/:search_id::search_template']
-    }
-  }
-  loaders: [
-    {
-      // load your services and directives
-      services: 'services.js',
-      directives: 'directives.js'
-  ]
-};
+/**
+ * This creates a new instance and initializes the global 
+ * app, controller, service variables.
+ */
+var Iot = new IonicBootstrapTabs('starter');
 
-IonicBootstrap.init(bootstrap, {log_states: true}, function(angular_app_instantiation_function) {
-  require(['some/more/stuff.js'], function() {
-    angular_app_instantiation_function();
-  });
+Iot.debug(true); // turn on for logging
+Iot.config();    // execute the default ionic configuration
+Iot.run();       // execute the default run code
+Iot.states({})   // load states for parsing and autoloading by require
+Iot.loaders([])  // other files from the js/lib or js/vendors to load
+
+// bootstrap ionic project, executes the code
+// that runs states, loads files etc...
+Iot.bootstrap();
+``
+
+###controllers
+
+Controller scripts go into js/controllers and you create them with the following variable `Controllers`:
+```javascript
+Controllers.controller('ControllerCtrl', function($scope) {
+    // ... some code
 });
-```
+``
+
+###services
+
+Services checked in /services directory and you create them with the following variable `Services`:
+```Javascript
+Services.service('Service', function() {
+    this.get = function () {
+
+    };
+});
+``
+
+###Factories and directories
+Factories can either fit in on file, stay with their respective service or in separate files.  Directories are also not inforced.
+
+
